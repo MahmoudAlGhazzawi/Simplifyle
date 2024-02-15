dc130343732eaa9fb1a5d3829412160337e32ca0
malghazzawi <mag@mayfair-hamburg.de>
2024-02-15 14:10:59 +0100

    Ordner erzeugen, Datei Löschen nach Upload

--- Backend/index.js
+++ Backend/index.js
@@ -2,22 +2,31 @@ const tesseract = require('tesseract.js');
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 
+// Falls Upload Ordner nicht existiert
+var dir = './uploads';
+if(!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
 
 app.use(express.static(path.join(__dirname + '/uploads')));
 
 app.set('view engine', 'ejs');
 
 
+var filePath = "";
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads');
     },
     filename: function (req, file, cb) {
+        filePath = file.fieldname + '-' + Date.now() + path.extname(file.originalname)
         cb(
             null,
-            file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+            filePath
         );
     },
 });
@@ -29,9 +38,18 @@ app.get('/', (req, res) => {
     res.render('index', {data:''}); 
 })
 
-app.post('/extractTextFromImage', upload.single('file'), async (req, res) => {
+app.post('/extractTextFromImage',  upload.single('file'), async (req, res) => {
     console.log(req.file.path); 
     const text = await tesseractConverter(req.file.path);
+
+    // delete File after processing it
+    fs.unlink(req.file.path, (err) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        // else File is removed
+      }) 
     res.render('index', {data:text})
 })
 
@@ -39,9 +57,6 @@ app.listen(3000, () => {
     console.log('ON PORT 3000')
 })
 
-
-
-// TODO: hier kommt Promise raus
 const tesseractConverter  = async (image) => {
   const worker = await tesseract.createWorker('eng');
   const ret = await worker.recognize(image);
