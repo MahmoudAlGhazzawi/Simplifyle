d5b9f79d3e009091db139159a6e7c13292cce18e
P-Oliomonte <hi@phillip-oehlenberg.com>
2024-02-22 12:43:45 +0100

    Updated useState von selectedLanguage

--- Frontend/src/settings.jsx
+++ Frontend/src/settings.jsx
@@ -10,16 +10,25 @@ const Settings = () => {
         setIsChecked(!isChecked);
     };
 
-    console.log(find())
+    const [selectedLanguage, setSelectedLanguage] = React.useState("");
+
+    const handleLanguageChange = (event) => {
+        setSelectedLanguage(event.target.value);
+    };
 
     return (
         <div className="container">
 
             <div className="picture-container"></div>
 
+            <div className="container-below">
+                <span className="page-counter">Page1/1</span>
+                <span className="redo"><i className="bi bi-trash"></i>Redo upload</span>
+            </div>
+
             <div className="select-container">
                 <p><i className="bi bi-globe"></i></p>
-                <select name="languages" id="languages">
+                <select name="languages" id="languages" value={selectedLanguage} onChange={handleLanguageChange}>
                     <option value="">Choose a language</option>
                     {find().map((obj) => {
                         return <option value={obj.name} key={obj.name} >{obj.name}</option>
